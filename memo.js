bcfc204c558ed576646a25a1d360496aff886106
Ali Yildiz <yildiz.ali.s1990@gmail.com>
2020-12-23 14:30:17 +0100

    jsLearnSession (else/if)

--- memo.js
+++ memo.js
@@ -57,7 +57,7 @@ const minutesPerHour = 60 ;
 
 const secondsPerMinute = 60 ;
 
-// == TYPE DE VARIABLE ==
+// == TYPE DE VARIABLE (primitifs) ==
 
 // number (nombre) ;
 
@@ -151,4 +151,102 @@ guests.unshift("Tao Perkington"); // "Tao Perkington" est ajouté au début du t
 guests.pop(); // supprimer le dernier élément du tableau
 
 
+//============================= PROGRAMME AVEC LA FONCTION MAIN ===
 
+// ------   Utilisez des valeurs  boolean   ------
+// En JavaScript, si on utilise des  boolean (bouléens, en français) simples pour les instructions
+//  if / else, la syntaxe se présente comme suit :
+
+if (myBoolean) {
+    // réaction à la valeur vraie de myBoolean
+} else {
+    // réaction à la valeur faux de myBoolean
+}
+
+//Donc, pour vérifier si un utilisateur est connecté, vous pouvez procéder comme suit :
+
+let userLoggedIn = true;
+
+if (userLoggedIn) {
+    console.log("Utilisateur connecté!");
+} else {
+    console.log("Alerte, intrus!");
+}
+
+//-----------  Utilisez des expressions ---------------
+
+//Les expressions de comparaison vous permettent de comparer deux valeurs par les opérateurs suivants :
+
+// <    inférieur à ;
+
+// <=   inférieur ou égal à ;
+
+// ==   égal à ;
+
+// >=    supérieur ou égal à ;
+
+// >    supérieur à ;
+
+// !=    différent de.
+
+
+const numberOfSeats = 30;
+let numberOfGuests = 25;
+
+if (numberOfGuests < numberOfSeats) {
+    // autoriser plus d'invités
+} else {
+    // ne pas autoriser de nouveaux invités
+}
+
+//Vous pouvez aussi chaîner les instructions  if /else  pour réagir à des conditions potentielles multiples :
+
+if (numberOfGuests == numberOfSeats) {
+    // tous les sièges sont occupés
+} else if (numberOfGuests < numberOfSeats) {
+    // autoriser plus d'invités
+} else {
+    // ne pas autoriser de nouveaux invités
+}
+//Le chaînage d'instructions permet de prévoir différents résultats en fonction des différentes situations.
+
+//---------------------  Comprenez les conditions multiples ---------------------
+
+//l'égalité simple vérifie la valeur, mais pas le type. Donc ceci renvoie la valeur  true  : 
+5 == "5"
+
+//par contre, l'égalité stricte vérifie à la fois la valeur et le type. Donc :
+5 === "5"
+//renvoie  false, car on compare un  number  à un  string. 
+//De même, il y a deux opérateurs d'inégalité  !=  et  !==, avec la même distinction.
+
+//  &&  — ET logique — pour vérifier si deux conditions sont toutes les deux vraies ;
+
+//  ||   — OU logique — pour vérifier si au moins une condition est vraie ;
+
+//  !   — NON logique — pour vérifier si une condition n'est pas vraie. 
+
+let userLoggedIn = true;
+let userHasPremiumAccount = true;
+let userHasMegaPremiumAccount = false;
+
+userLoggedIn && userHasPremiumAccount; // true
+userLoggedIn && userHasMegaPremiumAccount; // false
+
+userLoggedIn || userHasPremiumAccount; // true
+userLoggedIn || userHasMegaPremiumAccount; // true
+
+!userLoggedIn; // false
+!userHasMegaPremiumAccount; // true
+
+// EXEMPLE 
+if (minutesWatched === 0) {
+        this.watchedText = 'Not yet watched';
+        this.continueWatching = false;
+    } else if (minutesWatched > 0 && minutesWatched < duration) {
+        this.watchedText = 'watching';
+        this.continueWatching = true;
+    } else if (minutesWatched === duration) {
+        this.watchedText = 'Already watched';
+        this.continueWatching = false;
+    }
