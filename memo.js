e3fd555ab37b96b4277381800c4340d81231d077
Ali Yildiz <yildiz.ali.s1990@gmail.com>
2020-12-26 17:28:24 +0100

    jsLearnSession (while...:tandis que...)

--- memo.js
+++ memo.js
@@ -378,3 +378,18 @@ for (let episode of episodes) {
     episode.hasBeenWatched = false ;
 }
 
+
+//-------------- "WHILE = tandis que " -----------------Utilisez la boucle while pour continuer jusqu'à ce qu'on dise d'arrêter-----------------------
+let seatsLeft = 10;
+let passengersStillToBoard = 8;
+
+let passengersBoarded = 0;
+
+while (seatsLeft > 0 && passengersStillToBoard > 0) {
+    passengersBoarded++; // un passager embarque
+    passengersStillToBoard--; // donc il y a un passager de moins à embarquer
+    seatsLeft--; // et un siège de moins
+}
+
+console.log(passengersBoarded); // imprime 8, car il y a 8 passagers pour 10 sièges
+//Cette boucle  while  poursuit son exécution jusqu'à ce que l'un des nombres  seatsLeft  et  passengersStillToBoard  atteigne zéro, et à ce point elle se termine.
