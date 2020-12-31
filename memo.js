7de3acb6b282bad4c365ecde50975f283df4100e
Ali Yildiz <yildiz.ali.s1990@gmail.com>
2020-12-31 15:23:44 +0100

    jsLearnSession (recursivite)

--- memo.js
+++ memo.js
@@ -489,3 +489,24 @@ const sendWelcomeMessageToUser = (user) => {
 sendWelcomeMessageToUser(firstUser);
 sendWelcomeMessageToUser(secondUser);
 sendWelcomeMessageToUser(thirdUser);
+
+// ------------------- recursivité --------------
+
+const binarySearch = (array, thingToFind, start, end) => {
+    
+    if (start > end) {
+        return false;
+    }
+    
+    let mid = Math.floor((start + end) / 2);
+    
+    if (array[mid] === thingToFind) {
+        return true;
+    }
+    
+    if (thingToFind < array[mid]) {
+        return binarySearch(array, thingToFind, start, mid - 1);
+    } else {
+        return binarySearch(array, thingToFind, mid + 1, end);
+    }
+}
