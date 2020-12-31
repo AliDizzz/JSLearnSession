26592efcd3db149887c64ea0d4ae4495d1a79785
Ali Yildiz <yildiz.ali.s1990@gmail.com>
2020-12-31 14:55:41 +0100

    jsLearnSession (average function)

--- memo.js
+++ memo.js
@@ -393,3 +393,36 @@ while (seatsLeft > 0 && passengersStillToBoard > 0) {
 
 console.log(passengersBoarded); // imprime 8, car il y a 8 passagers pour 10 sièges
 //Cette boucle  while  poursuit son exécution jusqu'à ce que l'un des nombres  seatsLeft  et  passengersStillToBoard  atteigne zéro, et à ce point elle se termine.
+
+// -------------- fonction qui calcule la moyen  ----------
+
+// Modify the code here
+// ======================
+const calculateAverageRating = (ratings) => {
+
+    if(ratings.length === 0) {
+        return 0;
+    }
+    
+    let sum = 0;
+    for (let rating of ratings) {
+        sum += rating;
+    }
+    
+    return sum / ratings.length;
+
+    }
+
+  // ======================
+    
+    const tauRatings = [5, 4, 5, 5, 1, 2];
+    const colinRatings = [5, 5, 5, 4, 5];
+    
+    const tauAverage = calculateAverageRating(tauRatings);
+    const colinAverage = calculateAverageRating(colinRatings);
+    
+    if (tauAverage && colinAverage) {
+    document.querySelector('#tau-score').innerText = tauAverage.toFixed(1) + ' stars';
+    document.querySelector('#colin-score').innerText = colinAverage.toFixed(1) + ' stars';
+    document.querySelector('#clara-score').innerText = `${calculateAverageRating([]) === 0 ? 'No ratings' : calculateAverageRating([]) + ' stars'}`
+    }
